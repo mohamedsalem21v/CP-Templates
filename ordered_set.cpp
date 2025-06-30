5437753886b6b2d6b60b830dfd8159da7fcb5e2d
Mohammed Salem <mohamedsalem462006@gmail.com>
2025-06-30 15:52:27 +0300

    Update ordered_set.cpp

--- ordered_set.cpp
+++ ordered_set.cpp
@@ -7,24 +7,26 @@ template <class T>
 using ordered_set = tree<T, null_type, less<T>, rb_tree_tag,
 tree_order_statistics_node_update>;
 template <class T> 
-using multi_ordered_set = tree<T, null_type, less_equal<T>,
+using ordered_multiset = tree<T, null_type, less_equal<T>,
 rb_tree_tag, tree_order_statistics_node_update>;
 //order_of_key(n)  It returns the number of items that are strictly smaller than our item k in O(logn)
 //find_by_order(n)  It returns an iterator to the kth element (counting from zero) in the set in O(logn)
-#define endl "\n"
 #define sp " "
+#define endl "\n"
+const int MOD = 1e9+7;
 #define ll long long
 #define ld long double
 #define ull unsigned long long
-#define st string
+#define ve vector<int>
+#define vve vector<ve>
+#define de deque<int>
 #define pb(n) push_back(n)
 #define pf(n) push_front(n)
-#define ve vector<int>
-#define de deque<int>
-#define all(n) n.begin(),n.end()
-#define rall(n) n.rbegin(),n.rend()
+#define all(n) n.begin(), n.end()
+#define rall(n) n.rbegin(), n.rend()
 #define fixed(n) fixed << setprecision(n)
-
+#define input(v) for (auto &it : v) cin >> it
+#define output(v) for (auto &it : v) cout << it << " "
 void FastIo()
 {
     #ifndef ONLINE_JUDGE
@@ -36,17 +38,16 @@ void FastIo()
 
 void solve()
 {
-    
+
 }
 
 int main()
 {
     FastIo();
  	int test = 1;
-	cin >> test;
+	// cin >> test;
 	for(int tc = 1; tc <= test; tc++)
 	{
 		solve();
 	}
-    
 }
