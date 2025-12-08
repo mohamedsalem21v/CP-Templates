502509551dea176a3aaae7d55bf25f261d5a30cd
Mohammed Salem <mohamedsalem462006@gmail.com>
2025-12-08 23:46:35 +0200

    Refactor NumberTheory.cpp with improved structure

--- NumberTheory.cpp
+++ NumberTheory.cpp
@@ -1,18 +1,23 @@
 #include <bits/stdc++.h>
 #define sp " "
 #define endl "\n"
-#define st string
-const int MOD = 1e9+7;
 #define ll long long
+#define int long long
 #define ld long double
 #define ull unsigned long long
 #define ve vector<int>
-#define de deque<int>
-#define pb(n) push_back(n)
-#define pf(n) push_front(n)
 #define all(n) n.begin(), n.end()
 #define rall(n) n.rbegin(), n.rend()
 #define fixed(n) fixed << setprecision(n)
+#define debug cout << "\n======================\n"
+const int MOD = 1e9 + 7;
+const int inf = 1e18;
+#define input(v)       \
+for (auto &it : v) \
+cin >> it
+#define output(v)      \
+for (auto &it : v) \
+cout << it << " "
 using namespace std;
 void FastIo()
 {
@@ -64,36 +69,47 @@ bool isprime(ll n)
     return true;
 }
 //==================================================================
-const int N = 1e7+10;
-vector<bool> prime(N,true);
+const int N = 50 + 5;
+vector<bool> prime(N, true);
+vector<int> primeNumbers;
 void sieve()
 {
     prime[0] = false;
     prime[1] = false;
 
-    for(ll i=2;i*i<N;i++)
+    for (ll i = 2; i * i < N; i++)
     {
-        if(prime[i])
+        if (prime[i])
         {
-            for(ll j=i*i;j<N;j+=i)
+            for (ll j = i * i; j < N; j += i)
             {
                 prime[j] = false;
             }
         }
     }
+    for (ll i = 2; i < N; i++)
+    {
+        if (prime[i])
+            primeNumbers.push_back(i);
+    }
 }
-vector<bool>is_prime(N,1);vector<int>primes;
+vector<bool> is_prime(N, 1);
+vector<int> primes;
 void linearSieve()
 {
-    is_prime[0]= 0; is_prime[1]= 0;
-    for(ll i=2;i<N;i++)
+    is_prime[0] = 0;
+    is_prime[1] = 0;
+    for (ll i = 2; i < N; i++)
     {
-        if(is_prime[i]) primes.push_back(i);
-        for(auto it:primes)
+        if (is_prime[i])
+            primes.push_back(i);
+        for (auto it : primes)
         {
-            if(i*it>=N) break;
-            is_prime[i*it]=0;
-            if(i%it==0) break;
+            if (i * it >= N)
+                break;
+            is_prime[i * it] = 0;
+            if (i % it == 0)
+                break;
         }
     }
 }
@@ -102,41 +118,61 @@ ll powmod(ll x, ll y)
 {
     ll res = 1;
     x = x % MOD;
-    if (x == 0) return 0;
+    if (x == 0)
+        return 0;
     while (y > 0)
     {
         if (y & 1)
-            res = (res*x) % MOD;
+            res = (res * x) % MOD;
         y = y >> 1;
-        x = (x*x) % MOD;
+        x = (x * x) % MOD;
     }
     return res;
 }
-ll add(ll a,ll b)
+ll modinverse(ll a, ll mod)
 {
-    return ((a%MOD)+(b%MOD))%MOD;
+    return powmod(a, mod - 2);
 }
-ll mul(ll a,ll b)
+ll fastpower(ll x, ll y)
 {
-    return ((a%MOD)*(b%MOD))%MOD;
-}
-ll sub(ll a,ll b)
-{
-    return ((((a%MOD)-(b%MOD))%MOD)+MOD)%MOD;
-}
-ll divide(ll a,ll b)
-{
-    return mul(a,powmod(b,MOD-2));
+    ll result = 1;
+    while (y > 0)
+    {
+        if (y % 2)
+            result = result * x;
+        x *= x;
+        y /= 2;
+    }
+    return result;
 }
 //==================================================================
+ll add(ll a, ll b)
+{
+    return ((a % MOD) + (b % MOD)) % MOD;
+}
+ll mul(ll a, ll b)
+{
+    return ((a % MOD) * (b % MOD)) % MOD;
+}
+ll sub(ll a, ll b)
+{
+    return ((((a % MOD) - (b % MOD)) % MOD) + MOD) % MOD;
+}
+ll divide(ll a, ll b)
+{
+    return mul(a, powmod(b, MOD - 2));
+}
+//=================================<SOLVE>=================================
+
 void solve()
 {
-
+    
 }
 
-int main()
+signed main()
 {
     FastIo();
+    // sieve();
     int test = 1;
     // cin >> test;
     for (int tc = 1; tc <= test; tc++)
